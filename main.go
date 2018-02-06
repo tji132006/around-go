e9e4721c34a76c80d71e6d02f6e4166af15c2a07
unknown <tji132006@gmail.com>
2018-02-05 21:37:07 -0800

    add filter word function

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"reflect"
 	elastic "gopkg.in/olivere/elastic.v3"
 	"github.com/pborman/uuid"
+	"strings"
 )
 
 type Location struct {
@@ -170,7 +171,10 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		ps = append(ps, p)
+		if !containsFilteredWords(&p.Message) {
+			ps = append(ps, p)
+		}
+
 
 	}
 	js, err := json.Marshal(ps)
@@ -182,5 +186,17 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
-
 }
+func containsFilteredWords(s *string) bool {
+	filteredWords := []string{
+		"fuck",
+		"100",
+	}
+	for _, word := range filteredWords {
+		if strings.Contains(*s, word) {
+			return true
+		}
+	}
+	return false
+}
+
