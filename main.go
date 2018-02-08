595b6f91d3fb1a75f16b6319424c81849fb2a64c
unknown <tji132006@gmail.com>
2018-02-07 20:04:28 -0800

    add bigtable

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 	elastic "gopkg.in/olivere/elastic.v3"
 	"github.com/pborman/uuid"
 	"strings"
+	"context"
+	"cloud.google.com/go/bigtable"
+
 )
 
 type Location struct {
@@ -69,8 +72,9 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "possible-stock-194222"
+	BT_INSTANCE = "around-post"
+
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://104.196.0.160:9200"
 )
@@ -90,6 +94,34 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	id := uuid.New()
 	// Save to ES.
 	saveToES(&p, id)
+
+	//Save to BigTable
+
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// TODO (student questions) save Post into BT as well
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 }
 
@@ -174,8 +206,6 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		if !containsFilteredWords(&p.Message) {
 			ps = append(ps, p)
 		}
-
-
 	}
 	js, err := json.Marshal(ps)
 	if err != nil {
